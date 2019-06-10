32cfa52cea650a90d0dd848550ab13622b6c6be7
franvelastiqui <47385918+franvelastiqui@users.noreply.github.com>
2019-06-09 21:44:24 -0300

    Add files via upload

---
+++ Win_64/Employee.c
@@ -0,0 +1,47 @@
+#include "Employee.h"
+#include <stdlib.h>
+#include "string.h"
+
+Employee* employee_new()
+{
+    Employee* empleado;
+
+    empleado = (Employee*)malloc(sizeof(Employee));
+
+    return empleado;
+}
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr)
+{
+    char sueldo;
+
+    char* pSueldo=&sueldo;
+
+
+}
+
+employee_setId(Employee* empleado, int id)
+{
+    empleado->id=id;
+    return 1;
+}
+
+employee_setNombre(Employee* empleado, char nombre[])
+{
+    strcpy(empleado->nombre, nombre);
+    return 1;
+}
+
+employee_setHorasTrabajadas(Employee* empleado, int horasTrabajadas)
+{
+    empleado->horasTrabajadas=horasTrabajadas;
+    return 1;
+}
+
+employee_setSueldo(Employee* empleado, int sueldo)
+{
+    empleado->sueldo=sueldo;
+    return 1;
+}
+
+
---
+++ Win_64/parser.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+#include "string.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    Employee* empleado=employee_new();
+
+    char id[500], nombre[500], horasTrabajadas[500];
+
+    Employee* employee_newParametros(id, nombre, horasTrabajadas);
+
+    fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", id, nombre, horasTrabajadas, sueldo);
+
+    while(!feof(pFile))
+    {
+        fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", id, nombre, horasTrabajadas, sueldo);
+
+        employee_setId(empleado, atoi(id));
+        employee_setNombre(empleado, nombre);
+        employee_setHorasTrabajadas(empleado, atoi(horasTrabajadas));
+        employee_setSueldo(sueldo, atoi(sueldo));
+
+        ll_add(pArrayListEmployee, empleado);
+    }
+
+    return 1;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
